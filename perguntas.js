713904118f6b15e6418c84b166102c62d9577bb0
gpeca1 <g.bini@escola.pr.gov.br>
2025-06-09 07:44:09 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual a capital da Argentina?',
+    'Buenos Aires'
+)
+
+criaCartao(
+    'Matematica',
+    'O que e potencia?',
+    'Um numero multiplicado por ele mesmo a quantidade pedida'
+)
+
+criaCartao(
+    'Historia',
+    'Em que ano acabou a Segunda Guerra Mundial?',
+    '1945'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz amor em Inglês?',
+    'Amor em ingles é LOVE'
+)
+
